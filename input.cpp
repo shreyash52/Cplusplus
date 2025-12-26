70652a837af485e9367cfac5206448e7a9ce02b9
Shreyash Garje <shreyashgarje.5@gmail.com>
2025-12-26 19:21:17 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main (){
+
+       std :: string name ; 
+
+       int age; 
+
+       std :: cout << "whats your age ?" ; 
+        std :: cin >> age ; 
+
+
+         std :: cout << "whats the shrname of yours baddie ? : "; 
+         std :: getline(std :: cin >> std ::ws , name); 
+         std :: cin >> name ; '\n'; 
+   
+        std :: cout << "Hello""! " << name << '\n';  
+        std :: cout << "you are " << age << " old !!"  ;
+
+
+
+
+    return 0; 
+
+}
+
---
+++ mathfunction.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <cmath>
+
+int main(){ 
+
+    double x = 2.4 ; 
+    double y = 3 ; 
+    double z ; 
+
+
+
+        // z = std :: max (x, y );  (max ) 
+        // z= std :: min (x , y ) ;
+        //  z = pow(2, 4 );  
+        //z = sqrt (36) ; 
+        //    z  = abs (-3);
+        //  z = round(x); 
+        // z = ceil (x); 
+        z = floor (x); 
+        
+      
+        
+        std ::cout << z             ; 
+
+
+
+
+    return 0 ; 
+}
