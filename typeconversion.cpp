eb73d41424d35c6b0a4d2ddd4e379dce06228c93
Shreyash Garje <shreyashgarje.5@gmail.com>
2025-12-26 19:21:49 +0530

    Add files via upload

---
+++ typeconversion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+    int main(){
+
+        //   double x = (int)3.94; 
+        
+        //     char x = 100; 
+        // std:: cout << (char) 100 ;
+
+         int correct = 5 ;
+         int questions = 10; 
+         double result = (double )correct / (double)questions * 100; 
+
+        std :: cout << result << "%" ; 
+
+
+        return 0 ;
+    }
